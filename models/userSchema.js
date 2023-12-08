626706e98c1f34a9140b5124fd2a9fecbd18647a
Reshka <reshetnyuk79@gmail.com>
2023-12-08 19:16:00 +0200

    start models

--- models/userSchema.js
+++ models/userSchema.js
@@ -5,12 +5,15 @@ const userSchema = new mongoose.Schema(
   {
     password: {
       type: String,
-      required: [true, 'Set password for user'],
+      required: [true, 'Set a password for the user'],
+      minLength: 8,
     },
     email: {
       type: String,
       required: [true, 'Email is required'],
       unique: true,
+      maxLength: 100,
+      lowercase: true,
     },
     subscription: {
       type: String,
@@ -19,25 +22,39 @@ const userSchema = new mongoose.Schema(
     },
     avatarURL: String,
     token: String,
+    verify: {
+      type: Boolean,
+      default: false,
+    },
+    verificationToken: {
+      type: String,
+      required: [true, 'Verification token is required'],
+    },
   },
+
   { versionKey: false, timestamps: true }
 );
 
 const schema = Joi.object({
-  email: Joi.string().email().required(),
-  password: Joi.string().required(),
+  email: Joi.string().email().required().max(255),
+  password: Joi.string().required().min(8).max(50),
   subscription: Joi.string()
     .valid('starter', 'pro', 'business')
     .default('starter'),
 });
- 
+
 const updateSchema = Joi.object({
   subscription: Joi.string().valid('starter', 'pro', 'business').allow(null),
+});
+
+const verifySchema = Joi.object({
+  email: Joi.string().email().required(),
 });
 
 const schemas = {
   schema,
   updateSchema,
+  verifySchema,
 };
 
 const User = mongoose.model('user', userSchema);
