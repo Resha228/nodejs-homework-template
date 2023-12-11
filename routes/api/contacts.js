3dee4410f71a0d20774714dd07284940d5e8b3e8
Resha228 <123998484+Resha228@users.noreply.github.com>
2023-12-11 12:33:57 +0200

    edit-01

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,37 +1,22 @@
 const express = require('express');
-
 const queries = require('../../models/contacts');
-
 const { bodyValidator } = require('../../middleware/middleware');
-
 const { joiSchema, addToFavorites } = require('../../models/joi');
+const isValidId = require('../../middleware/isValidId'); 
 
 const router = express.Router();
 
 const CONTACTS_PATH = '/';
-
 const CONTACT_ID_PATH = '/:contactId';
-
 const FAVORITE_PATH = '/:contactId/favorite';
 
+
 router.get(CONTACTS_PATH, queries.listContacts);
-
-router.get(CONTACT_ID_PATH, queries.getContactById);
-
+router.get(CONTACT_ID_PATH, isValidId, queries.getContactById);
 router.post(CONTACTS_PATH, bodyValidator(joiSchema), queries.addContact);
-
-router.delete(CONTACT_ID_PATH, queries.removeContact);
-
-router.put(
-  CONTACT_ID_PATH,
-  bodyValidator(joiSchema),
-  queries.updateContact
-);
-
-router.patch(
-  FAVORITE_PATH,
-  bodyValidator(addToFavorites),
-  queries.addToFavorites
-);
+router.delete(CONTACT_ID_PATH, isValidId, queries.removeContact);
+router.put(CONTACT_ID_PATH, bodyValidator(joiSchema), isValidId, queries.updateContact);
+router.patch(FAVORITE_PATH, bodyValidator(addToFavorites), isValidId, queries.addToFavorites);
 
 module.exports = router;
+
